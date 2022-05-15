7eaab3bcb82f5c4fbcbed5a5746da127de0e0e6a
Florencia Feijoó <florencciafeijoo@gmail.com>
2022-05-15 00:33:25 -0300

    03 JS: DOM, eventos y local storage

--- js/app.js
+++ js/app.js
@@ -12,6 +12,59 @@ const productsDOM = document.querySelector(".products-center");
 //cart
 let cart = [];
 
+const setCartValues = cart => {
+    let tempTotal = 0;
+    let itemsTotal = 0; 
+    cart.map(item => {
+        tempTotal += item.price * item.amount;
+        itemsTotal += item.amount; 
+    });
+    cartTotal.innerText = tempTotal;
+    cartItems.innerText = itemsTotal;
+}
+
+const addCartItem = item => {
+    let div = document.createElement("div");
+    div.className = "cart-item";
+    div.innerHTML = `
+    <img src="${item.image}" alt="Producto">
+    <div>
+        <h4>${item.title}</h4>
+        <h5>${item.price} USD</h5>
+        <span class="remove-item" data-id=${item.id}>Remove</span>
+    </div>
+    <div>
+        <i class="fas fa-chevron-up" data-id=${item.id}></i>
+        <p class="item-amount">${item.amount}</p>
+        <i class="fas fa-chevron-down" data-id=${item.id}></i>
+    </div>
+    `;
+    cartContent.appendChild(div);
+    console.log(cartContent);
+}
+
+const showCart = () => {
+    cartOverlay.classList.add("transparent-bcg");
+    cartDOM.classList.add("show-cart");
+}
+
+const hideCart = () => {
+    cartOverlay.classList.remove("transparent-bcg");
+    cartDOM.classList.remove("show-cart");
+}
+
+const setupAPP = () => {
+    cart = getCart();
+    setCartValues(cart);
+    populateCart(cart);
+    cartBtn.addEventListener("click", showCart);
+    closeCartBtn.addEventListener("click", hideCart);
+}
+
+const populateCart = cart => {
+    cart.forEach(item => addCartItem(item));
+}
+
 //local storage
 const productsList =   [{id: 1, title: "Sushi salad", price: 8, image: "images/product-1.jpg"},
                         {id: 2, title: "Pasta", price: 4, image: "images/product-2.jpg"},
@@ -25,6 +78,10 @@ const productsList =   [{id: 1, title: "Sushi salad", price: 8, image: "images/p
 localStorage.setItem("productsList", JSON.stringify(productsList));
 
 const products = JSON.parse(localStorage.getItem("productsList"));
+
+const getCart = () => {
+    return localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : [];
+}
 
 //display products
 for (const product of products) {
@@ -40,46 +97,36 @@ for (const product of products) {
     </div>
     <h3>${product.title}</h3>
     <h4>${product.price} USD</h4>
-    `
+    `;
     productsDOM.appendChild(displayProducts);
 }
 
-//show cart
-cartBtn.addEventListener("click", () => {
-    cartOverlay.classList.add("transparent-bcg");
-    cartDOM.classList.add("show-cart");
-});
-
-//hide cart
-closeCartBtn.addEventListener("click", () => {
-    cartOverlay.classList.remove("transparent-bcg");
-    cartDOM.classList.remove("show-cart");
-});
-
 //add to cart
+setupAPP();
+
 const bagButtons = [...document.querySelectorAll(".bag-btn")];
 
 bagButtons.forEach(btn => {
-    btn.addEventListener("click", () => {
-        cartOverlay.classList.add("transparent-bcg");
-        cartDOM.classList.add("show-cart");
-    });   
-}); 
-
-bagButtons.forEach(btn => {
     let id = btn.dataset.id;
-    let inCart = cart.find(product => product.id === id);
+    let inCart = cart.find(product => product.id == id);
     if (inCart) {
         btn.innerText = "Agregado";
         btn.disabled = true;
-    } else {
-        btn.addEventListener("click", e => {
-            e.target.innerText = "Agregado";
-            e.target.disabled = true;
-            
-            //get product from products
-            let cartItem = products.find(product => product.id === id); 
-            console.log(cartItem); 
-        });
-    }
+    } 
+    btn.addEventListener("click", e => {
+        e.target.innerText = "Agregado";
+        e.target.disabled = true;
+        //get product from products
+        let cartItem = {...products.find(product => product.id == id), amount: 1}; 
+        //add product to cart
+        cart = [...cart, cartItem];
+        //save cart in local storage
+        localStorage.setItem("cart", JSON.stringify(cart));
+        //set cart values
+        setCartValues(cart);
+        //display cart item
+        addCartItem(cartItem);
+        //show cart
+        showCart();
+    });
 });
